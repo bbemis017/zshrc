32ab49534f78c265334fc1d3bf943cc3b85668a0
Benjamin Bemis <bbemis@alumni.purdue.edu>
2020-04-04 18:13:23 -0500

    Added deploy and install

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Update zshrc in root
+cp ${ZSHRC_SRC_DIR}/.zshrc ~/.zshrc
---
+++ install.sh
@@ -0,0 +1,3 @@
+
+# Installs oh-my-zsh, https://github.com/ohmyzsh/ohmyzsh
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
